b0cd29e27cc9cacc5b55c56f8c35ecab3b1370f1
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-04 20:49:07 -0300

    test(functional): updated functional setup to destroy collections after test

--- test/functional/config/setup.js
+++ test/functional/config/setup.js
@@ -1,1 +1,22 @@
 /* eslint-env node, mocha */
+const mongoose = require('mongoose')
+
+const {
+  HOST,
+  PORT,
+  PROTOCOL
+} = require('../../../src/env')
+
+beforeEach(function (done) {
+  this.baseURL = `${PROTOCOL}://${HOST}:${PORT}`
+  require('../../../index')
+  done()
+})
+
+afterEach(function (done) {
+  if (mongoose.connection.db) {
+    mongoose.connection.db.dropDatabase(done)
+  } else {
+    done()
+  }
+})
