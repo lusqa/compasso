3e51484d137e1bd75fcd7ecff31b6bfe4a55f228
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 20:33:56 -0300

    test(client): added unit tests to get-clients.service

---
+++ test/unit/specs/services/client/get-clients.service.spec.js
@@ -0,0 +1,70 @@
+/* eslint-env node, mocha */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+
+const getClientsService = require('~src/services/client/get-clients.service')
+
+const { expect } = chai
+
+describe('get-cities.service', () => {
+  describe('When the service ran succesfully', () => {
+    it('And I pass the name as the parameter, should returns succesfully', async () => {
+      const clients = [
+        {
+          name: 'Lucas Gomes',
+          sex: 'M',
+          birthDate: '1970-12-17',
+          age: 20,
+          city: 'Fortaleza'
+        },
+        {
+          name: 'Maria José',
+          sex: 'F',
+          birthDate: '1970-05-12',
+          age: 20,
+          city: 'Fortaleza'
+        }
+      ]
+
+      const Client = {
+        find: () => ({
+          populate: () => ({
+            skip: () => ({
+              limit: () => ({
+                sort: () => {
+                  return new Promise((resolve, reject) => (resolve([clients[0]])))
+                }
+              })
+            })
+          })
+        })
+      }
+
+      const name = 'Lucas Gomes'
+      const result = await getClientsService(Client, { name })
+
+      expect(result[0].name).to.be.equal(clients[0].name)
+      expect(result[0].state).to.be.equal(clients[0].state)
+    })
+  })
+
+  describe('When the service ran with an error', () => {
+    it('Should rethrows the error', () => {
+      const error = new Error('Error creating city')
+      const Client = {
+        find: () => ({
+          skip: () => ({
+            limit: () => ({
+              sort: () => {
+                throw error
+              }
+            })
+          })
+        })
+      }
+
+      expect(getClientsService(Client, {})).to.be.rejectedWith(error)
+    })
+  })
+})
