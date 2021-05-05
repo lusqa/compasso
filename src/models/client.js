19af4caebfb2145dc4de4c9a0aa937d32e3a3cf8
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 00:41:17 -0300

    feat(client): added client model

---
+++ src/models/client.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true
+    },
+    sex: {
+      type: String,
+      required: true
+    },
+    birthDate: {
+      type: Date,
+      required: true
+    },
+    age: {
+      type: Date,
+      required: true
+    },
+    city: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'City'
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+)
+
+module.exports = mongoose.model('Client', schema, 'client')
