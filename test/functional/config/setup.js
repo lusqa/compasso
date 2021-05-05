46cd953e264c4249e40137b495d63065ce26b2b3
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 00:51:05 -0300

    fix: moved mockgoose logic to the functional tests setup

--- test/functional/config/setup.js
+++ test/functional/config/setup.js
@@ -1,5 +1,6 @@
 /* eslint-env node, mocha */
 const mongoose = require('mongoose')
+const { Mockgoose } = require('mockgoose')
 
 const {
   HOST,
@@ -7,8 +8,30 @@ const {
   PROTOCOL
 } = require('~src/env')
 
+const LOGGER = require('~src/logger')([__filename].join())
+
+const connectDatabase = async () => {
+  LOGGER.info('Using mockgoose to run tests')
+  const mockgoose = new Mockgoose(mongoose)
+
+  LOGGER.info('Preparing mockgoose storage!')
+  await mockgoose.prepareStorage()
+  LOGGER.info('Mockgoose storage prepared!')
+
+  await mongoose.connect('mongodb://example.com/TestingDB', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  LOGGER.info('Connection to mock database has successfully established!')
+}
+
 before(async function () {
   global.baseURL = `${PROTOCOL}://${HOST}:${PORT}/api/v1`
+
+  await connectDatabase()
+
   const createServer = require('../../../server')
   await createServer()
 })
