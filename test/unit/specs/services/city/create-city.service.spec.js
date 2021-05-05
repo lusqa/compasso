86968e32aebabd004b84f1dd2aecf867a7f51c09
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-04 21:13:46 -0300

    test(unit): added tests to create-city.service

---
+++ test/unit/specs/services/city/create-city.service.spec.js
@@ -0,0 +1,46 @@
+/* eslint-env node, mocha */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+
+const createCityService = require('../../../../../src/services/city/create-city.service')
+
+const { expect } = chai
+
+describe('create-city.service', () => {
+  it('When create method returns successfully, the create-city function should returns sucessfully too', async () => {
+    const date = new Date().toISOString()
+    const City = {
+      create: ({ name, state }) => {
+        return {
+          name,
+          state,
+          createdAt: date,
+          updatedAt: date
+        }
+      }
+    }
+
+    const name = 'Fortaleza'
+    const state = 'Ceará'
+
+    const result = await createCityService(City, { name, state })
+
+    expect(result.name).to.be.equal(name)
+    expect(result.state).to.be.equal(state)
+    expect(result.createdAt).to.be.equal(date)
+    expect(result.updatedAt).to.be.equal(date)
+  })
+
+  it('When create method returns an error, the create-city function should returns throw an error too', () => {
+    const error = new Error('Error creating city')
+    const City = {
+      create: () => {
+        throw error
+      }
+    }
+
+    expect(createCityService(City, {})).to.be.rejectedWith(error)
+  })
+})
