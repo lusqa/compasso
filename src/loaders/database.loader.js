5c2d39fa03aa233b0ae5cf21a24c6434c7f4c3a7
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 02:51:03 -0300

    Revert "test: updated code to run test to a test database"
    This reverts commit 577d3be568005d2660f4d47b644e578220e39f2c.

--- src/loaders/database.loader.js
+++ src/loaders/database.loader.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const LOGGER = require('../logger')([__filename].join())
-const { DATABASE_URL } = require('../env')
+const { NODE_ENV, DATABASE_URL } = require('../env')
 
 const databaseLoader = async () => {
   const options = {
@@ -12,8 +12,10 @@ const databaseLoader = async () => {
   }
 
   try {
-    await mongoose.connect(DATABASE_URL, options)
-    LOGGER.info('Connection to database engine has successfully established')
+    if (NODE_ENV !== 'test') {
+      await mongoose.connect(DATABASE_URL, options)
+      LOGGER.info('Connection to database engine has successfully established')
+    }
   } catch (err) {
     LOGGER.error('Something went wrong connecting to the database!')
     throw err
--- test/functional/config/setup.js
+++ test/functional/config/setup.js
@@ -1,5 +1,6 @@
 /* eslint-env node, mocha */
 const mongoose = require('mongoose')
+const { Mockgoose } = require('mockgoose')
 
 const {
   HOST,
@@ -9,12 +10,29 @@ const {
 
 const LOGGER = require('~src/logger')([__filename].join())
 
+const connectDatabase = async () => {
+  LOGGER.info('Using mockgoose to run tests')
+  const mockgoose = new Mockgoose(mongoose)
+
+  LOGGER.info('Preparing mockgoose storage!')
+  await mockgoose.prepareStorage()
+  LOGGER.info('Mockgoose storage prepared!')
+
+  await mongoose.connect('mongodb://example.com/TestingDB', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  LOGGER.info('Connection to mock database has successfully established!')
+}
+
 before(async function () {
   global.baseURL = `${PROTOCOL}://${HOST}:${PORT}/api/v1`
 
-  const createServer = require('../../../server')
+  await connectDatabase()
 
-  LOGGER.info('Initializating test server...')
+  const createServer = require('../../../server')
   await createServer()
 })
 
