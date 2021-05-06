ad461175b5b1613adf5b8cee67df0a09dfeb36ff
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 21:07:10 -0300

    test(client): updated get-clients.service unit test to use sinon.fake and spy database methods

--- test/unit/specs/services/client/get-clients.service.spec.js
+++ test/unit/specs/services/client/get-clients.service.spec.js
@@ -1,50 +1,75 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
+const sinon = require('sinon')
 
 const getClientsService = require('~src/services/client/get-clients.service')
 
 const { expect } = chai
+chai.use(chaiAsPromised)
 
 describe('get-cities.service', () => {
   describe('When the service ran succesfully', () => {
-    it('And I pass the name as the parameter, should returns succesfully', async () => {
-      const clients = [
-        {
-          name: 'Lucas Gomes',
-          sex: 'M',
-          birthDate: '1970-12-17',
-          age: 20,
-          city: 'Fortaleza'
-        },
-        {
-          name: 'Maria José',
-          sex: 'F',
-          birthDate: '1970-05-12',
-          age: 20,
-          city: 'Fortaleza'
-        }
-      ]
+    describe('And I pass a parameter', () => {
+      it('Should returns succesfully and return database object', async () => {
+        const clients = [
+          {
+            name: 'Lucas Gomes',
+            sex: 'M',
+            birthDate: '1970-12-17',
+            age: 20,
+            city: 'Fortaleza'
+          },
+          {
+            name: 'Maria José',
+            sex: 'F',
+            birthDate: '1970-05-12',
+            age: 20,
+            city: 'Fortaleza'
+          }
+        ]
 
-      const Client = {
-        find: () => ({
+        const Client = {
+          find: () => ({
+            populate: () => ({
+              skip: () => ({
+                limit: () => ({
+                  sort: () => {
+                    return new Promise((resolve, reject) => (resolve([clients[0]])))
+                  }
+                })
+              })
+            })
+          })
+        }
+
+        const name = 'Lucas Gomes'
+        const result = await getClientsService(Client, { name })
+
+        expect(result[0].name).to.be.equal(clients[0].name)
+        expect(result[0].state).to.be.equal(clients[0].state)
+      })
+
+      it('Should returns succesfully and pass the correct params to the database methods', async () => {
+        const find = sinon.fake.returns({
           populate: () => ({
             skip: () => ({
               limit: () => ({
                 sort: () => {
-                  return new Promise((resolve, reject) => (resolve([clients[0]])))
+                  return new Promise((resolve, reject) => (resolve([])))
                 }
               })
             })
           })
         })
-      }
 
-      const name = 'Lucas Gomes'
-      const result = await getClientsService(Client, { name })
+        const Client = { find }
 
-      expect(result[0].name).to.be.equal(clients[0].name)
-      expect(result[0].state).to.be.equal(clients[0].state)
+        const name = 'Lucas Gomes'
+
+        await getClientsService(Client, { name })
+        expect(find.firstArg).to.have.property('name')
+        expect(find.firstArg).to.be.deep.equal({ name })
+      })
     })
   })
 
