f3e5a27bec34d4d8e3c47a0f10800e5fea72a18f
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 22:02:52 -0300

    test(client): added functional tests to get clients endpoint

--- test/functional/specs/controllers/client.controller.spec.js
+++ test/functional/specs/controllers/client.controller.spec.js
@@ -2,6 +2,7 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 
 const City = require('~src/models/city')
+const Client = require('~src/models/client')
 
 chai.use(chaiHttp)
 const { expect } = chai
@@ -53,6 +54,44 @@ describe('city.controller', () => {
         expect(response.body[0].birthDate).to.includes(birthDate)
         expect(response.body[0].age).to.be.equal(age)
         expect(response.body[0].city.name).to.be.be.equal(city)
+      })
+    })
+  })
+
+  describe('When getting a city', () => {
+    let client
+    beforeEach(async () => {
+      const city = await City.create({ name: 'Fortaleza', state: 'Ceará' })
+      client = await Client.create({
+        name: 'Lucas Gomes',
+        sex: 'M',
+        birthDate: '1970-12-17',
+        age: 20,
+        city: city._id
+      })
+    })
+
+    describe('And pass the query parameters correctly', () => {
+      it('It should return 200', async () => {
+        const response = await chai
+          .request(global.baseURL)
+          .get('/client')
+          .query({ name: client.name })
+
+        expect(response.status).to.be.equal(200, 'The status code should be 200')
+      })
+
+      it('It should return the correct data', async () => {
+        const response = await chai
+          .request(global.baseURL)
+          .get('/client')
+          .query({ name: client.name })
+
+        expect(response.status).to.be.equal(200, 'The status code should be 200')
+
+        expect(response.body[0].name).to.be.equal(client.name)
+        expect(response.body[0].sex).to.be.equal(client.sex)
+        expect(response.body[0].age).to.be.equal(client.age)
       })
     })
   })
