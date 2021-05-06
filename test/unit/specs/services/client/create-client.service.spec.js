3c2d2b0efea7d09bc4801fdd303bdc32fa26c4c4
Lucas Gomes <lucasdeoliveira887@gmail.com>
2021-05-05 21:01:09 -0300

    test(client): updated create-client.service unit tests to use sinon to spy model methods

--- test/unit/specs/services/client/create-client.service.spec.js
+++ test/unit/specs/services/client/create-client.service.spec.js
@@ -1,39 +1,89 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
+const sinon = require('sinon')
 
 const createClientService = require('~src/services/client/create-client.service')
 
+chai.use(chaiAsPromised)
 const { expect } = chai
 
 describe('create-client.service', () => {
   describe('When the service ran succesfully', () => {
-    it('And the client city was found, should returns succesfully', async () => {
-      const cityId = 'city-id'
+    describe('And the client city was found', () => {
+      it('Should returns succesfully and return database object', async () => {
+        const cityId = 'city-id'
+        const City = {
+          findOne: ({ name }) => {
+            return new Promise((resolve, reject) => (resolve({ _id: cityId, name })))
+          }
+        }
+
+        const Client = {
+          create: ({ name, sex, birthDate, age, city }) => {
+            return new Promise((resolve, reject) => (resolve({ name, sex, birthDate, age, city })))
+          }
+        }
+
+        const name = 'Lucas Gomes'
+        const sex = 'M'
+        const birthDate = '1970-12-17'
+        const age = 20
+        const city = 'Fortaleza'
+
+        const result = await createClientService(Client, City, { name, sex, birthDate, age, city })
+        expect(result.name).to.be.equal(name)
+        expect(result.sex).to.be.equal(sex)
+        expect(result.birthDate).to.be.equal(birthDate)
+        expect(result.age).to.be.equal(age)
+        expect(result.city).to.be.equal(cityId)
+      })
+
+      it('Should returns succesfully and pass the correct params to the database methods', async () => {
+        const cityId = 'fake-id'
+        const cityFindOne = sinon.fake.resolves({ _id: cityId })
+        const City = {
+          findOne: cityFindOne
+        }
+
+        const clientCreate = sinon.fake()
+        const Client = {
+          create: clientCreate
+        }
+
+        const name = 'Lucas Gomes'
+        const sex = 'M'
+        const birthDate = '1970-12-17'
+        const age = 20
+        const city = 'Fortaleza'
+
+        await createClientService(Client, City, { name, sex, birthDate, age, city })
+
+        expect(cityFindOne.firstArg).to.have.property('name')
+        expect(cityFindOne.firstArg).to.be.deep.equal({ name: city })
+
+        expect(clientCreate.firstArg).to.have.property('name', name)
+        expect(clientCreate.firstArg).to.have.property('sex', sex)
+        expect(clientCreate.firstArg).to.have.property('birthDate', birthDate)
+        expect(clientCreate.firstArg).to.have.property('city', cityId)
+        expect(clientCreate.firstArg).to.be.deep.equal({ name, sex, birthDate, age, city: cityId })
+      })
+    })
+
+    it('And the client city was not found, should throws an error', () => {
       const City = {
         findOne: ({ name }) => {
-          return new Promise((resolve, reject) => (resolve({ _id: cityId, name })))
+          return new Promise((resolve, reject) => (resolve()))
         }
       }
 
       const Client = {
-        create: ({ name, sex, birthDate, age, city }) => {
-          return new Promise((resolve, reject) => (resolve({ name, sex, birthDate, age, city })))
+        create: ({ city }) => {
+          return new Promise((resolve, reject) => (resolve({ city })))
         }
       }
 
-      const name = 'Lucas Gomes'
-      const sex = 'M'
-      const birthDate = '1970-12-17'
-      const age = 20
       const city = 'Fortaleza'
-
-      const result = await createClientService(Client, City, { name, sex, birthDate, age, city })
-      expect(result.name).to.be.equal(name)
-      expect(result.sex).to.be.equal(sex)
-      expect(result.birthDate).to.be.equal(birthDate)
-      expect(result.age).to.be.equal(age)
-      expect(result.city).to.be.equal(cityId)
+      expect(createClientService(Client, City, { city })).to.be.rejected
     })
 
     it('And the client city was not found, should throws an error', () => {
